10c639494712676911a03ae4d6964d45af70f28f
Alan K <aknipmeyer@gmail.com>
2020-01-15 15:36:41 +0000

    Increase timeout (#15)
    * increased timeout from 60 to 120
    
    * upddate ecosystem.config.js

--- app/ecosystem.config.js
+++ app/ecosystem.config.js
@@ -10,7 +10,7 @@ module.exports = {
       script    : "/NATS/bin/DQ_NATS_file_ingest",
       interpreter: "python",
       env: {
-        PROCESS_INTERVAL: 60,
+        PROCESS_INTERVAL: 120,
         MAX_BATCH_SIZE: 5000,
         SSH_REMOTE_HOST : process.argv[5],
         SSH_REMOTE_USER : process.argv[6],
